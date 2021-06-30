9e8e4668fb064b769c327c2d664f4410c0c27298
Mayara Lima <may.veloso.lima@gmail.com>
2021-06-29 22:10:02 -0300

    Exercicios realizados na aula do dia 29/06

---
+++ src/dia1/Main.java
@@ -0,0 +1,19 @@
+package dia1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean verdadeiroOuFalso = false;
+        char caracterSimples = 'a';
+        byte baite = 127;
+        short inteiroCurto = 32767;
+        int inteiro = 2_147_483_647;
+        long longo = 2_147_483_647;
+        float pontoFluante = 2.9f;
+        double pontoFluanteComMaiorPrecisao = 2.90;
+
+        short idade = 18;
+        System.out.println(idade);
+    }
+}
