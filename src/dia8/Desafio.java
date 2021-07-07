fc7ecad43b12e4d0b3e09a302464b1830b5fced2
Mayara Lima <may.veloso.lima@gmail.com>
2021-07-06 22:34:58 -0300

    Resolução melhorada do desafio

--- src/dia8/Desafio.java
+++ src/dia8/Desafio.java
@@ -1,30 +1,26 @@
 package dia8;
 
-import javax.imageio.ImageTranscoder;
-
 public class Desafio {
 
     public static void desafio() {
         //Dado um array de números inteiros com valores negativos e positivos,
         //encontro o número mais próximo de zero. Se houver valores como 2 e -2, considere o número positivo.
 
-        int[] numeros = {-1, -2, 3, 4, -5, 8};
+        int[] numeros = {-1, -2, 3, 4, -5, 1};
 
         int distanciaMenor = Integer.MAX_VALUE;
+        int posicao = 0;
 
         for (int i = 0; i < numeros.length; i++) {
 
-            if (Math.abs(numeros[i]) == Math.abs(distanciaMenor) && numeros[i] > distanciaMenor) {
+            if (Math.abs(numeros[i]) < distanciaMenor) {
                 distanciaMenor = numeros[i];
-            } else {
-                if (Math.abs(numeros[i]) < Math.abs(distanciaMenor)) {
-                    distanciaMenor = numeros[i];
-                }
+                posicao = i;
             }
 
         }
 
-        System.out.println(distanciaMenor);
+        System.out.println(numeros[posicao]);
     }
 
     public static void main(String[] args) {
